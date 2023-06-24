924ae5f66cf6a210f9e8a7571c51021093afa39f
Mahamed-Kamal <mahamedkamall47@gmail.com>
2023-06-24 16:53:16 +0300

    + hadeth details screen

--- lib/ui/home/hadeth/hadeth_title.dart
+++ lib/ui/home/hadeth/hadeth_title.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HadethTitleWidget extends StatelessWidget {
-  String title;
+import '../../hadeth_details/hadeth_details.dart';
+import 'hadeth_model_class.dart';
 
-  HadethTitleWidget(this.title);
+class HadethTitleWidget extends StatelessWidget {
+  Hadeth hadeth;
+
+  HadethTitleWidget(this.hadeth);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          HadethDetails.routeName,
+          arguments: hadeth,
+        );
+      },
       child: Container(
-        child: Text(title,
+        child: Text(hadeth.title,
             style: TextStyle(fontSize: 24), textAlign: TextAlign.center),
       ),
     );
